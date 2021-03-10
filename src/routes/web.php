e96576c42320e1e6497eee12e1be6e4685798d9c
ahmedmohamed24 <ahmed@ahmed.localdomain>
2021-03-10 23:10:25 +0200

    [add-feature] require jwt and implement it in User model

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/token', function () {
+    return csrf_token();
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
