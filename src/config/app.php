7fecd389e5423bee063b3697631e98db65072412
ahmed162721 <ahmedmohamed24.dev@gmail.com>
2022-02-11 15:47:22 +0200

    solve telescope loading in production

--- src/config/app.php
+++ src/config/app.php
@@ -167,7 +167,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
 
         Intervention\Image\ImageServiceProvider::class,
     ],
